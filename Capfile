e7b1a05fdc9bd4501d3a108e42f41a882b16140e
Alexandr <ano@cyclad.pl>
2017-07-02 00:31:27 +0200

    nginx and puma capistrano added

--- Capfile
+++ Capfile
@@ -11,5 +11,6 @@ require 'capistrano/bundler'
 # require "bundler/capistrano"
 require 'capistrano/rails/migrations'
 require 'capistrano/rails/assets'
+require 'capistrano/puma'
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -12,52 +12,87 @@ set :rvm_ruby_version, "2.3.1@izifood"
 set :deploy_to, "/home/izifood_app/"
 set :use_sudo, false
 set :rails_env, "production"
-set :deploy_via, :copy
-set :ssh_options, { :forward_agent => true }
+set :deploy_via, :copy #:remote_cache
+set :ssh_options, { :forward_agent => true }#, keys: %w(~/.ssh/id_rsa.pub)
+set :pty, true
+set :use_sudo, false
+set :stage, :production
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+#Puma config
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
 # You can configure the Airbrussh format using :format_options.
 # These are the defaults.
 # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
-# Default value for :pty is false
-set :pty, true
+## Defaults:
+# set :scm,           :git
+# set :branch,        :master
+# set :format,        :pretty #:airbrussh
+# set :log_level,     :debug
+# set :keep_releases, 5
 
-# Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml", "config/application.yml"
+## Linked Files & Directories (Default None):
+set :linked_files, %w{config/database.yml, config/secrets.yml, config/application.yml, config/nginx.conf.erb}
+# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-set :keep_releases, 5
-namespace :deploy do
-
-	task :setup_config do 
-		on roles(:app) do
-			# execute "ln -s #{current_path}/config/ngix.conf /etc/nginx/sites-enabled/izifood_app.conf"
-	    end
-	end
-    before 'deploy:started', 'deploy:setup_config'
-    task :start do
-        on roles(:app) do
-		    execute "bundle exec pumactl -P /home/deploy/.pids/puma.pid restart"
-	    end
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
     end
-    after "deploy:cleanup", "deploy:start"
+  end
+
+  before :start, :make_dirs
 end
-# namespace :bundler do
-#   desc "run bundle install and ensure all gem requirements are met"
-#   task :install do
-#     # run "cd #{current_path} && rvm use 2.3.1@izifood && bundle install  --without=test"
-#   end
-# end
-# before "deploy:restart", "bundle:install"
+
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      execute("sudo ln -nfs #{shared_path}config/config/nginx.conf.erb' '/etc/nginx/sites-enabled/izifood_app'")
+      execute('sudo service nginx restart')
+
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+end
+
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
