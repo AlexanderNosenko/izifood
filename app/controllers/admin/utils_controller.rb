20bd6baec445a1d6696f388dded944f642d29bff
Alexandr <ano@cyclad.pl>
2017-10-22 12:43:28 +0200

    [+] Delivery slots admin updates

--- app/controllers/admin/utils_controller.rb
+++ app/controllers/admin/utils_controller.rb
@@ -5,4 +5,10 @@ class Admin::UtilsController < Admin::ApplicationController
     IngredientMatchesWorker.perform_async()
     redirect_to admin_search_duplicates_path
   end
+
+  def update_slots
+    DeliverySlot.update_slots
+    redirect_to admin_delivery_slots_path
+  end
+  
 end
