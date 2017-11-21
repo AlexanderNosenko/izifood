b962ae3e5772b71868f3f07e92778dad822bd91f
Alexandr <ano@cyclad.pl>
2017-11-21 01:31:28 +0100

    [*] Production fixes

--- db/migrate/20171103230058_create_recipes_tags.rb
+++ db/migrate/20171103230058_create_recipes_tags.rb
@@ -6,6 +6,6 @@ class CreateRecipesTags < ActiveRecord::Migration[5.1]
 
       t.timestamps
     end
-    add_index :recipe_tags, [:recipe, :tag], unique: true
+    add_index :recipes_tags, [:recipe_id, :tag_id], unique: true
   end
 end
